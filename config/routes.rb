fd96eeeb1386433ba2d2226bc7d1735fbf5e816f
Jonas Ajubi <jonas+github@giantmonkey.de>
2023-02-21 16:29:29 +0100

    almost finished

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get 'tasks/new', to: 'tasks#new'
+
   get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  post 'tasks', to: 'tasks#create'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
